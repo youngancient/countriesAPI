50c2851228d66ee76def00cb51325d69361e4392
Jude Tochy <judetochyokoye@gmail.com>
2023-03-10 16:49:22 +0100

    i added animations, still subject to editing tho

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,9 +1,35 @@
 import { useContext, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { NavLink, useNavigate } from "react-router-dom";
 import ThemeContext from "../../Utils/ThemeContext";
 import "./style.css";
+import { motion } from "framer-motion";
 
-const Country = ({ capital, name, population, region, flag}) => {
+const countryVariants = {
+  initial: {
+    scale: 0,
+    opacity: 0,
+  },
+  final: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
+// const underVariants = {
+//   initial: {
+//     height: 0,
+//   },
+//   final: {
+//     height: "auto",
+//     transition: {
+//       duration: 1.2,
+//     },
+//   },
+// };
+const Country = ({ capital, name, population, region, flag }) => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate(`/${name}`);
@@ -11,28 +37,47 @@ const Country = ({ capital, name, population, region, flag}) => {
 
   const themes = useContext(ThemeContext);
   return (
-    <div className={`country ${themes.shadow}`} onClick={handleClick}>
+    <motion.div
+      className={`country ${themes.shadow}`}
+      onClick={handleClick}
+      variants={countryVariants}
+      viewport={{ once: true }}
+      initial="initial"
+      whileInView="final"
+    >
       <div className="country-img">
-        <img src={flag} alt={name} className="" loading="lazy" />
+        {/* <img src={flag} alt={name} className="" loading="lazy" /> */}
+        <LazyLoadImage
+          src={flag}
+          alt={name}
+          effect="blur"
+          width={320}
+          height={200}
+          placeholderSrc="/placeholder.jpg"
+        />
       </div>
       <div className={`under ${themes.componentBG} ${themes.primaryText}`}>
-        <h3>{name}</h3>
-        <div className="minor">
-          <span>
-            <p className="head">Population:</p>
-            <p className={`value ${themes.minorText}`}>{population}</p>
-          </span>
-          <span>
-            <p className="head">Region:</p>
-            <p className={`value ${themes.minorText}`}>{region}</p>
-          </span>
-          <span>
-            <p className="head">Capital:</p>
-            <p className={`value ${themes.minorText}`}>{capital === undefined ? "None found" : capital.toString()}</p>
-          </span>
+        <div>
+          <h3>{name}</h3>
+          <div className="minor">
+            <span>
+              <p className="head">Population:</p>
+              <p className={`value ${themes.minorText}`}>{population}</p>
+            </span>
+            <span>
+              <p className="head">Region:</p>
+              <p className={`value ${themes.minorText}`}>{region}</p>
+            </span>
+            <span>
+              <p className="head">Capital:</p>
+              <p className={`value ${themes.minorText}`}>
+                {capital === undefined ? "None found" : capital.toString()}
+              </p>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
